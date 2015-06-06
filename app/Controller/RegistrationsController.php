b20ff99a5c6fbe9fae01e670ba1abe9d25b9be75
ikrasavin <ikrasavin@users.noreply.github.com>
2015-06-06 09:28:58 -0500

    up in rank

--- app/Controller/RegistrationsController.php
+++ app/Controller/RegistrationsController.php
@@ -1582,7 +1582,7 @@ function checkIn2( $event_id = null ){
 									case 'Up in age':
 										$up_a = 1;
 										break;
-									case 'Up in skill':
+									case 'Up in rank':
 										$up_s = 1;
 										break;
 								}
