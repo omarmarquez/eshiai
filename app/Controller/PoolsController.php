199b3fa77de3c62998e3339a18be2d465ad1f9db
ikrasavin <ikrasavin@users.noreply.github.com>
2018-06-08 11:13:23 -0500

    hide awards when not ready v2
    Hide when sent to head table only

--- app/Controller/PoolsController.php
+++ app/Controller/PoolsController.php
@@ -1133,7 +1133,7 @@ WHERE p.id =$id";
 			}
 			}
 		}
-		if( $pt == 'rr' && (($pstat == 5) || ($pstat == 6) || ($pstat == 8) )){
+		if( $pt == 'rr' && (($pstat == 5) || ($pstat == 6) || ($pstat == 8) || ($pstat == 9))){
 				/*
 				$seed = $r['seed'];
 				$this->Ntkfdf->set_value( "tot_win_$seed",  $r['match_wins'] );
@@ -1152,7 +1152,7 @@ WHERE p.id =$id";
 		}
 
 
-		if( ($pstat == 5) || ($pstat == 6) || ($pstat == 8) ){
+		if( ($pstat == 5) || ($pstat == 6) || ($pstat == 8) || ($pstat == 9) ){
 			// completed or awarded
 
 		$rcond = array( 'Registration.bracket_pos <>' => 0 );
